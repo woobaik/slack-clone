05e7348b30ae2ced5a3e43d0b94f63568c5f8bd9
Jbaik <joungwoo.baik@gmail.com>
2020-07-22 14:49:23 -0700

    Install react-router dom, and react-icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import "./App.css"
+
+import Login from "./components/Auth/Login"
+import Registration from "./components/Auth/Registration"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/login">
+						<Login />
+					</Route>
+					<Route exact path="/registration">
+						<Registration />
+					</Route>
+					<Route exact path="/"></Route>
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Auth/Registration.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Registration = () => {
+	return <div>registartaion page</div>
+}
+
+export default Registration
