fd4679825d9161a3c178648708d48328e00ac5e1
Jbaik <joungwoo.baik@gmail.com>
2020-07-29 20:48:16 -0700

    Add dynamic channel highlight

--- src/components/MainPage/LeftPanel/Channels/Channels.jsx
+++ src/components/MainPage/LeftPanel/Channels/Channels.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react"
 import "./Channels.style.scss"
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io"
-import { FaSlackHash } from "react-icons/fa"
+import { FaSlackHash, FaQq } from "react-icons/fa"
 import { Transition } from "react-transition-group"
 
 const Channels = () => {
 	const [channelListOpen, setChannelListOpen] = useState(false)
-
+	const [activeChannel, setActiveChannel] = useState("")
 	const duration = 200
 
 	const defaultStyle = {
@@ -21,6 +21,12 @@ const Channels = () => {
 		exited: { opacity: 0, visibility: "hidden" },
 	}
 
+	const list = [
+		{ title: "Music", uId: 0 },
+		{ title: "faq", uId: 1 },
+		{ title: "office", uId: 2 },
+		{ title: "job search", uId: 3 },
+	]
 	return (
 		<div className="Channel">
 			<div
@@ -38,34 +44,19 @@ const Channels = () => {
 							...transitionStyles[state],
 						}}>
 						<ul>
-							<li>
-								<div className="channel-title">
-									<FaSlackHash />
-									<span>music</span>
-								</div>
-								<div className="channel-counter">13</div>
-							</li>
-							<li>
-								<div className="channel-title">
-									<FaSlackHash />
-									<span>nyc office</span>
-								</div>
-								<div className="channel-counter">3</div>
-							</li>
-							<li className="active">
-								<div className="channel-title">
-									<FaSlackHash />
-									<span>job search</span>
-								</div>
-								<div className="channel-counter">3</div>
-							</li>
-							<li>
-								<div className="channel-title">
-									<FaSlackHash />
-									<span>faq</span>
-								</div>
-								<div className="channel-counter">3</div>
-							</li>
+							{list.map((item) => {
+								return (
+									<li
+										onClick={() => setActiveChannel(item.uId)}
+										className={activeChannel === item.uId ? "active" : ""}>
+										<div className="channel-title">
+											<FaSlackHash />
+											<span>{item.title}</span>
+										</div>
+										<div className="channel-counter"></div>
+									</li>
+								)
+							})}
 						</ul>
 					</div>
 				)}
